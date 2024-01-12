75ae03264684e8146e877e6ad23a1da331b5fb23
tre3p <whereareyo@yahoo.com>
2024-01-12 12:14:54 +0300

    init project

---
+++ src/main/kotlin/com/tre3p/randomizedjpgdownloader/RandomizedJpgDownloaderApplication.kt
@@ -0,0 +1,11 @@
+package com.tre3p.randomizedjpgdownloader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RandomizedJpgDownloaderApplication
+
+fun main(args: Array<String>) {
+	runApplication<RandomizedJpgDownloaderApplication>(*args)
+}
---
+++ src/test/kotlin/com/tre3p/randomizedjpgdownloader/RandomizedJpgDownloaderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tre3p.randomizedjpgdownloader
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RandomizedJpgDownloaderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
