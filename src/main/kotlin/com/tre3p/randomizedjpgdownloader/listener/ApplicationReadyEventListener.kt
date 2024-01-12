7185c06a02a300abea3f18590284d1de36647720
tre3p <whereareyo@yahoo.com>
2024-01-12 17:54:09 +0300

    rename method

--- src/main/kotlin/com/tre3p/randomizedjpgdownloader/listener/ApplicationReadyEventListener.kt
+++ src/main/kotlin/com/tre3p/randomizedjpgdownloader/listener/ApplicationReadyEventListener.kt
@@ -1,15 +1,15 @@
 package com.tre3p.randomizedjpgdownloader.listener
 
-import com.tre3p.randomizedjpgdownloader.service.RandomizedJpgDownloaderService
+import com.tre3p.randomizedjpgdownloader.service.RandomizedImageDownloaderService
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
-class ApplicationReadyEventListener(private val jpgDownloaderService: RandomizedJpgDownloaderService) {
+class ApplicationReadyEventListener(private val jpgDownloaderService: RandomizedImageDownloaderService) {
 
     @EventListener(ApplicationReadyEvent::class)
     suspend fun launchJpgDownloading() {
-        jpgDownloaderService.launchJpegsDownloading()
+        jpgDownloaderService.launchImagesDownloading()
     }
 }
