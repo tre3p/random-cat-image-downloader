36339a7f82bc3181742d001d07e27ba87210c426
tre3p <whereareyo@yahoo.com>
2024-01-12 17:54:03 +0300

    return entire response entity

--- src/main/kotlin/com/tre3p/randomizedjpgdownloader/service/DownloaderService.kt
+++ src/main/kotlin/com/tre3p/randomizedjpgdownloader/service/DownloaderService.kt
@@ -1,13 +1,13 @@
 package com.tre3p.randomizedjpgdownloader.service
 
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 
 @Service
 class DownloaderService(private val restTemplate: RestTemplate) {
-    fun download(url: String): ByteArray {
+    fun download(url: String): ResponseEntity<ByteArray> {
         val response = restTemplate.getForEntity(url, ByteArray::class.java)
-        return response.body ?: throw RestClientException("Response body is null")
+        return response
     }
 }
