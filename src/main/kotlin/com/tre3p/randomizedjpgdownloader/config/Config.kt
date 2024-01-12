f0bb93845f384e94cd535312ed14a92242b13789
tre3p <whereareyo@yahoo.com>
2024-01-12 16:07:43 +0300

    add RestTemplate config and DownloaderService

---
+++ src/main/kotlin/com/tre3p/randomizedjpgdownloader/config/Config.kt
@@ -0,0 +1,11 @@
+package com.tre3p.randomizedjpgdownloader.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class Config {
+    @Bean
+    fun restTemplate(): RestTemplate = RestTemplate()
+}
