877fc5c763e27a421051d387af6f09f685c10f95
tre3p <whereareyo@yahoo.com>
2024-02-03 17:31:17 +0300

    #4 move logic of adding double to separate class. add singlethreaded and multithreaded tests

---
+++ src/main/kotlin/com/tre3p/randomizedjpgdownloader/misc/AtomicDouble.kt
@@ -0,0 +1,25 @@
+package com.tre3p.randomizedjpgdownloader.misc
+
+import java.util.concurrent.atomic.AtomicLong
+
+/**
+ * Implementation of AtomicDouble class based on storing IEEE 754 floating-point bit layout in AtomicLong variable
+ *
+ * More on this: https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/atomic/package-summary.html
+ *               https://docs.oracle.com/javase/8/docs/api/java/lang/Double.html#doubleToRawLongBits-double-
+ *               https://docs.oracle.com/javase/8/docs/api/java/lang/Double.html#longBitsToDouble-long-
+ */
+class AtomicDouble(initValue: Double = 0.0) {
+
+    private val value: AtomicLong = AtomicLong(initValue.toRawBits())
+
+    fun addAndGet(toAddValue: Double): Double {
+        return Double.fromBits(this.value.addDoubleBits(toAddValue))
+    }
+
+    fun get(): Double {
+        return Double.fromBits(this.value.get())
+    }
+
+    private fun AtomicLong.addDoubleBits(double: Double) = this.updateAndGet { prevVal -> (Double.fromBits(prevVal) + double).toRawBits() }
+}
